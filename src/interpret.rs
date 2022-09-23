59f328902627e15a18d0b8bf996b29d169cc1cd6
Dworv <dwarvyt@gmail.com>
2022-09-22 19:37:47 -0700

    docs: add documentation and future framework

--- src/interpret.rs
+++ src/interpret.rs
@@ -1,2 +1,21 @@
+use crate::{State, Instruction};
 
-pub fn run() {}
+/// Parses raw Brainfuck code into list of instructions.
+pub fn parse(raw: &String) -> Vec<Instruction> {
+    todo!()
+}
+
+/// Executes a list of Brainfuck instructions.
+pub fn execute(instructions: Vec<Instruction>) -> State {
+    todo!()
+}
+
+/// Run Brainfuck code.
+pub fn run(raw: &String) -> State {
+    todo!()
+}
+
+/// Run Brainfuck code from a previous state.
+pub fn run_from_state(raw: &String, previous: State) -> State {
+    todo!()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+pub mod interpret;
+
+pub use interpret::{run, run_from_state};
+
+/// A `Vec` of `u8`s representing a the memory
+/// of a Brainfuck process.
 pub type Memory = Vec<u8>;
+/// The pointer that indicates the selected cell 
+/// of a Brainfuck process.
 pub type Pointer = u32;
 
 /// The state of a Brainfuck process.
@@ -21,21 +29,47 @@ impl State {
 
 /// A Brainfuck instruction.
 pub enum Instruction {
-    /// +
+    /// Represents the `+` instruction.
+    /// 
+    /// Used to increment the currect cell.
     Increment,
-    /// -
+    /// Represents the `-` instruction.
+    /// 
+    /// Used to decrement the currect cell.
     Decrement,
-    /// >
+    /// Represents the `>` instruction.
+    /// 
+    /// Used to move to the next cell.
     Forward,
-    /// <
+    /// Represents the `<` instruction.
+    /// 
+    /// Used to increment the previous cell.
     Backward,
-    /// [
-    StartLoop,
-    /// ]
-    EndLoop
+    /// Represents a loop (inside `[]`).
+    /// 
+    /// Repeats everything within it for as long as the pointer 
+    /// starts on a non-null cell. 
+    Loop(Vec<Instruction>),
+    /// Represents the end of a Brainfuck loop.
+    /// 
+    /// Only used for parsing, not present in a parsed list
+    /// of instructions.
+    LoopEnd,
+    /// Represents the `.` instruction.
+    /// 
+    /// Used to increment the previous cell.
+    Out,
+    /// Represents the `,` instruction.
+    /// 
+    /// Opens stdin and takes the first character of the inputted 
+    /// string.
+    In
 }
 
 impl Instruction {
+    /// Parses a character into a Brainfuck instruction.
+    /// 
+    /// If the input is not an instruction, 
     pub fn from_char (c: char) -> Option<Self> {
         use Instruction::*;
         Some(match c {
@@ -43,8 +77,8 @@ impl Instruction {
             '-' => Decrement,
             '>' => Forward,
             '<' => Backward,
-            '[' => StartLoop,
-            ']' => EndLoop,
+            '[' => Loop(vec![]),
+            ']' => LoopEnd,
             _ => { return None }
         })
     }
