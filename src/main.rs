864c1a15285a8a7c446f23cef1b6fa88d4269204
Dworv YT <dwarvyt@gmail.com>
2022-09-23 22:34:00 -0700

    feat: improve cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::path::PathBuf;
+
 use clap::Parser;
 
-use bf::parse;
+use bf::{run, run_from_state};
 
 /// A brainfuck interpreter.
 #[derive(Parser, Debug)]
@@ -8,7 +10,7 @@ use bf::parse;
 struct Cli {
     /// Filename of .b file. If not used, opens repl.
     #[clap(value_parser)]
-    filename: Option<String>,
+    filename: Option<PathBuf>,
     /// Reveal all memory after running.
     #[clap(short, long)]
     reveal: bool,
@@ -20,7 +22,15 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
 
-    let parsed = parse(&String::from("+[->>[--]><-]++[>>>]..,"));
+    match cli.filename {
+        Some(fp) => {
+            // INTERPRET FILE
+        },
+        None => {
+            let finished = run(&String::from("+++>>>---"));
 
-    println!("{:?}", parsed)
+            println!("{:?}", finished)
+        },
+    }
+    
 }
